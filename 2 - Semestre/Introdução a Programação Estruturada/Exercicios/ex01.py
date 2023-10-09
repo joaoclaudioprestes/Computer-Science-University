4104fc35129acba70f0fb2e9c6023f0fe626c91e
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2023-10-09 20:51:23 -0300

    update

--- 2 - Semestre/Introdução a Programação Estruturada/Exercicios/ex01.py
+++
@@ -1,9 +0,0 @@
-print('----- Calculo da função y -----')
-print()
-print('A função a ser realizada a seguir é: f(x) = 3x + 2')
-print()
-x = float(input('Digite o valor a ser calculado considerando o o X da função acima: '))
-y = 3*x + 2
-print()
-print('-----RESULTADO-----')
-print(f'O valor de "y" da função em domínio real é {y}')
