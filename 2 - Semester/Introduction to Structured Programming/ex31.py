d9d8b4facce52933e5afc86bcef733add712f350
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-03-02 15:14:46 -0300

    update

---
+++ 2 - Semester/Introduction to Structured Programming/ex31.py
@@ -0,0 +1,11 @@
+def espaco_tempo(espaco,tempo):
+    espaco_tempo = espaco/tempo
+    
+    print(f'O valor do epaço tempo é: {espaco_tempo}')
+
+espaco = int(input('Digite o espaço: '))
+tempo = int(input('Digite o tempo: '))
+
+espaco_tempo(espaco,tempo)
+
+#   
